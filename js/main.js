61876ae5ae70ba0a33e19aa7f7faf847bd31c5e2
Eric Hendrix <agentmrbig@gmail.com>
2019-05-07 01:31:40 -0400

    fixed fb login button styling issue, hiding canvase until logged in

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
+/**-------------------------------------------------------------
+ *  Initialization
+ * -------------------------------------------------------------
+ */
+// init on dom load
+document.addEventListener('DOMContentLoaded', init);
 
+// init app
+function init(){
+    const canvas = document.getElementById("gameScreen");
+    const logRegForm = document.getElementById('loginBox');
+}
+
+/**-------------------------------------------------------------
+ *  FACEBOOK 
+ * -------------------------------------------------------------
+ */
 // hide login form if logged in
 function setElements(isLoggedIn){
     if(isLoggedIn){
         document.getElementById('loginBox').style.display = 'none';
+        document.getElementById("gameScreen").style.display = 'flexbox';
+        
 
     }else{
         document.getElementById('loginBox').style.display = 'flexbox';
+        document.getElementById("gameScreen").style.display = 'none';
     }
 }
